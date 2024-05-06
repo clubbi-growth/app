692b2b04e34ea21f73c5e95436c9e98cad8e3eac
clubbi-growth <165522968+clubbi-growth@users.noreply.github.com>
2024-05-06 12:44:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -248,5 +248,329 @@ def load_concorrencia():
 
 query_concorrencia = load_concorrencia()
  
-query_concorrencia
+
+# %% Query Users Previsao
+# Query Users Previsão
+print("Query  Users Previsão") 
+
+ 
+query_users_prev =  '''select * from public.ops_customer '''
+
+
+@st.cache_resource( ttl = 43200) 
+def load_users_previsao():
+    cursor = load_redshift()
+    cursor.execute(query_users_prev)
+    df_users: pd.DataFrame = cursor.fetch_dataframe()  
+
+    df_users['Tipo_Cliente'] = np.where(
+    
+                                        (df_users['size']== 'counter') |  
+                                        (df_users['size']== 'one_checkout') |  
+                                        (df_users['size']== 'two_checkouts') | 
+                                        (df_users['size']== 'three_to_four_checkouts'),
+                                        '1-4 Cxs' , '5-9 Cxs' )
+
+
+
+    df_users['1-4 Cxs'] = np.where((df_users['Tipo_Cliente'] == '1-4 Cxs') , 1 , 0 )
+    df_users['5-9 Cxs'] = np.where((df_users['Tipo_Cliente'] == '1-4 Cxs') , 0 , 1 )
+    df_users = df_users.rename(columns = {'region name':'Region Name'})  
+    df_users['Não_Mercado'] =  np.where((df_users['tipo da loja'] == 'Mercado' ) ,   0, 1  )
+
+
+    return df_users
+
+ 
+df_users_previsao =  load_users_previsao()
+
+
+
+# %% Query Orders 
+# Query Orders 
+ 
+print("Query Orders - df_orders")
+
+query_order_d_1  = "select \
+DATE_FORMAT(ord.order_datetime,'%Y-%m-%d %H:00:00') as DateHour,\
+Date(ord.order_datetime) as Data,\
+HOUR(ord.order_datetime) as Hora,\
+CONVERT(ord.id, char) as order_id,\
+CONVERT(ord_ite.id, char) as order_item_id,\
+ord.customer_id, \
+ord.region_id, \
+CASE WHEN cli.region_id in (1,7,19,27,28,29,30,31,36,37)  THEN 'RJC' \
+WHEN cli.region_id in (22,24,25) THEN 'RJI' \
+WHEN cli.region_id in (16,26,49,50,51,52,53) THEN 'BAC' ELSE ord.region_id END as 'Região',\
+ord_ite.store_id, \
+CONVERT(ord_ite.product_id, CHAR) as ean,\
+CONVERT(ord_ite.unit_product_id, CHAR) as unit_ean,\
+prod.description as Produto,\
+ord_ite.category as Categoria,\
+ord_ite.is_multi_package,\
+ord_ite.product_package_qtd,\
+ord_ite.price_managers,\
+Convert(ord_ite.offer_id, char) as offer_id,  \
+case when ord_ite.original_price > ord_ite.unit_price then 1 else 0 end as flag_desconto,\
+case when  ord_ite.is_multi_package = 0 then  COALESCE(ord_ite.original_price,ord_ite.unit_price) else COALESCE(ord_ite.original_price,ord_ite.unit_price)/ ord_ite.product_package_qtd  end as Original_Price, \
+case when  ord_ite.is_multi_package = 0 then  ord_ite.unit_price else  ord_ite.unit_price/ ord_ite.product_package_qtd  end as Price, \
+case when ord_ite.original_price > ord_ite.unit_price then \
+case when  ord_ite.is_multi_package = 0 then  COALESCE(ord_ite.original_price,ord_ite.unit_price) else COALESCE(ord_ite.original_price,ord_ite.unit_price)/ ord_ite.product_package_qtd end - \
+case when  ord_ite.is_multi_package = 0 then  ord_ite.unit_price else  ord_ite.unit_price/ ord_ite.product_package_qtd  end \
+else 0 end as desconto_unitario, \
+case when ord_ite.original_price > ord_ite.unit_price then \
+(case when  ord_ite.is_multi_package = 0 then  COALESCE(ord_ite.original_price,ord_ite.unit_price) else COALESCE(ord_ite.original_price,ord_ite.unit_price)/ ord_ite.product_package_qtd end - \
+case when  ord_ite.is_multi_package = 0 then  ord_ite.unit_price else  ord_ite.unit_price/ ord_ite.product_package_qtd  end ) * \
+case \
+when ord_ite.product_package_qtd  is null then ord_ite.quantity \
+when ord_ite.product_package_qtd  <=0 then ord_ite.quantity \
+else ord_ite.product_package_qtd  * ord_ite.quantity end \
+else 0 end as desconto_total, \
+case \
+when ord_ite.product_package_qtd  is null then ord_ite.quantity \
+when ord_ite.product_package_qtd  <=0 then ord_ite.quantity \
+else ord_ite.product_package_qtd  * ord_ite.quantity end as Quantity, \
+ord_ite.quantity *  \
+(CASE WHEN prod.gross_weight_in_gram IS NOT NULL THEN prod.gross_weight_in_gram  WHEN prod.net_volume_in_liters IS NOT NULL AND cat.gross_weight_per_content_volume_liter IS NOT NULL THEN COALESCE(prod.number_of_items, 1) * prod.net_volume_in_liters * cat.gross_weight_per_content_volume_liter  \
+WHEN prod.net_weight_in_gram IS NOT NULL AND cat.gross_weight_per_net_weight_gram IS NOT NULL THEN COALESCE(prod.number_of_items, 1) * prod.net_weight_in_gram * cat.gross_weight_per_net_weight_gram  \
+WHEN cat.category_id IS NOT NULL THEN COALESCE(cat.average_without_outliers, cat.average) * COALESCE(prod.number_of_items, 1)  \
+WHEN prod.net_weight_in_gram IS NOT NULL THEN prod.net_weight_in_gram  \
+WHEN prod.net_volume_in_liters IS NOT NULL THEN prod.net_volume_in_liters * 1000 ELSE 1000  END) / 1000.0 as 'Peso',\
+ord_ite.total_price  as 'Gmv' \
+from  clubbi_backend.order ord \
+left join clubbi_backend.order_item ord_ite on ord_ite.order_id = ord.id and (ord_ite.is_cancelled = 0 or ord_ite.is_cancelled IS NULL) \
+left join clubbi.product prod ON ord_ite.product_id = prod.ean \
+left join  clubbi.merchants  cli on cli.client_site_code = ord.customer_id \
+left join clubbi.category_volume cat ON prod.category_id = cat.category_id  \
+where    \
+1 = 1 \
+and DATE(ord.order_datetime) >= '2024-01-01' \
+and DATE(ord.order_datetime) <  CURDATE()  \
+;"\
+
+ 
+query_order_d_0  = "select \
+DATE_FORMAT(ord.order_datetime,'%Y-%m-%d %H:00:00') as DateHour,\
+Date(ord.order_datetime) as Data,\
+HOUR(ord.order_datetime) as Hora,\
+CONVERT(ord.id, char) as order_id,\
+CONVERT(ord_ite.id, char) as order_item_id,\
+ord.customer_id, \
+ord.region_id, \
+CASE WHEN cli.region_id in (1,7,19,27,28,29,30,31,36,37)  THEN 'RJC' \
+WHEN cli.region_id in (22,24,25) THEN 'RJI' \
+WHEN cli.region_id in (16,26,49,50,51,52,53) THEN 'BAC' ELSE ord.region_id END as 'Região',\
+ord_ite.store_id, \
+CONVERT(ord_ite.product_id, CHAR) as ean,\
+CONVERT(ord_ite.unit_product_id, CHAR) as unit_ean,\
+prod.description as Produto,\
+ord_ite.category as Categoria,\
+ord_ite.is_multi_package,\
+ord_ite.product_package_qtd,\
+ord_ite.price_managers,\
+Convert(ord_ite.offer_id, char) as offer_id,  \
+case when ord_ite.original_price > ord_ite.unit_price then 1 else 0 end as flag_desconto,\
+case when  ord_ite.is_multi_package = 0 then  COALESCE(ord_ite.original_price,ord_ite.unit_price) else COALESCE(ord_ite.original_price,ord_ite.unit_price)/ ord_ite.product_package_qtd  end as Original_Price, \
+case when  ord_ite.is_multi_package = 0 then  ord_ite.unit_price else  ord_ite.unit_price/ ord_ite.product_package_qtd  end as Price, \
+case when ord_ite.original_price > ord_ite.unit_price then \
+case when  ord_ite.is_multi_package = 0 then  COALESCE(ord_ite.original_price,ord_ite.unit_price) else COALESCE(ord_ite.original_price,ord_ite.unit_price)/ ord_ite.product_package_qtd end - \
+case when  ord_ite.is_multi_package = 0 then  ord_ite.unit_price else  ord_ite.unit_price/ ord_ite.product_package_qtd  end \
+else 0 end as desconto_unitario, \
+case when ord_ite.original_price > ord_ite.unit_price then \
+(case when  ord_ite.is_multi_package = 0 then  COALESCE(ord_ite.original_price,ord_ite.unit_price) else COALESCE(ord_ite.original_price,ord_ite.unit_price)/ ord_ite.product_package_qtd end - \
+case when  ord_ite.is_multi_package = 0 then  ord_ite.unit_price else  ord_ite.unit_price/ ord_ite.product_package_qtd  end ) * \
+case \
+when ord_ite.product_package_qtd  is null then ord_ite.quantity \
+when ord_ite.product_package_qtd  <=0 then ord_ite.quantity \
+else ord_ite.product_package_qtd  * ord_ite.quantity end \
+else 0 end as desconto_total, \
+case \
+when ord_ite.product_package_qtd  is null then ord_ite.quantity \
+when ord_ite.product_package_qtd  <=0 then ord_ite.quantity \
+else ord_ite.product_package_qtd  * ord_ite.quantity end as Quantity, \
+ord_ite.quantity *  \
+(CASE WHEN prod.gross_weight_in_gram IS NOT NULL THEN prod.gross_weight_in_gram  WHEN prod.net_volume_in_liters IS NOT NULL AND cat.gross_weight_per_content_volume_liter IS NOT NULL THEN COALESCE(prod.number_of_items, 1) * prod.net_volume_in_liters * cat.gross_weight_per_content_volume_liter  \
+WHEN prod.net_weight_in_gram IS NOT NULL AND cat.gross_weight_per_net_weight_gram IS NOT NULL THEN COALESCE(prod.number_of_items, 1) * prod.net_weight_in_gram * cat.gross_weight_per_net_weight_gram  \
+WHEN cat.category_id IS NOT NULL THEN COALESCE(cat.average_without_outliers, cat.average) * COALESCE(prod.number_of_items, 1)  \
+WHEN prod.net_weight_in_gram IS NOT NULL THEN prod.net_weight_in_gram  \
+WHEN prod.net_volume_in_liters IS NOT NULL THEN prod.net_volume_in_liters * 1000 ELSE 1000  END) / 1000.0 as 'Peso',\
+ord_ite.total_price  as 'Gmv' \
+from  clubbi_backend.order ord \
+left join clubbi_backend.order_item ord_ite on ord_ite.order_id = ord.id and (ord_ite.is_cancelled = 0 or ord_ite.is_cancelled IS NULL) \
+left join clubbi.product prod ON ord_ite.product_id = prod.ean \
+left join  clubbi.merchants  cli on cli.client_site_code = ord.customer_id \
+left join clubbi.category_volume cat ON prod.category_id = cat.category_id  \
+where    \
+1 = 1 \
+and DATE(ord.order_datetime) >= CURDATE() \
+and DATE(ord.order_datetime) <= '2025-04-01'  \
+;"\
+
+
+
+ 
+
+@st.cache_resource( ttl = 43200) 
+def load_orders_d_1():
+    mydb = load_my_sql()
+    data = pd.read_sql(query_order_d_1,mydb) 
+    return data
+
+
+ 
+
+@st.cache_resource( ttl = 600) # ttl = 30 Minutos = 60 segundos x 30 = 1800 segundos  
+def orders_final():
+    mydb = load_my_sql()
+    
+    
+    df_produtos = load_produtos() 
+
+    df_produtos['ean'] = df_produtos['ean'].astype(np.int64).astype(str)
+    df_produtos = df_produtos.rename(columns={'ean':'unit_ean_prod','description':'Unit_Description'})[['unit_ean_prod','Unit_Description','Categoria']] 
+
+    df_orders_d_1 = load_orders_d_1()
+    df_orders_d_0 = pd.read_sql(query_order_d_0,mydb) 
+    df_orders = pd.concat([df_orders_d_1, df_orders_d_0 ] )
+    df_orders['DateHour'] = pd.to_datetime(df_orders['DateHour'])   
+
+
+    df_orders = df_orders.drop(columns = ['Categoria'])
+    df_orders = df_orders.merge(df_produtos  ,how ='left', left_on='unit_ean', right_on='unit_ean_prod', suffixes=(False, False))
+    
+    df_orders['Produtos'] = df_orders['unit_ean_prod'].astype(str) + ' - ' +  df_orders['Unit_Description'].astype(str) 
+
+    return df_orders
+ 
+ 
+df_orders = orders_final() 
+
+@st.cache_resource( ttl = 600) # ttl = 30 Minutos = 60 segundos x 30 = 1800 segundos  
+def hora_orders_final():
+    data_atual = datetime.date.today()
+    hora_atual = datetime.datetime.now()  - timedelta(hours=3)   
+
+    data_formatada = data_atual.strftime('%d/%m/%Y')
+    hora_formatada = hora_atual.strftime('%H:%M:%S')
+    print(f"Data: {data_formatada}")
+    print(f"Hora: {hora_formatada}")  
+
+
+    data_hora_completa = f"{data_formatada} {hora_formatada}"
+    return data_hora_completa
+
+
+hora_atualizacao = hora_orders_final() 
+ 
+date_hour_orders = df_orders[df_orders['Data'] == df_orders['Data'].max()]['DateHour'].max() - timedelta(hours=1)    
+max_hora_orders = df_orders[df_orders['Data'] == df_orders['Data'].max()]['Hora'].max() -1 
+if  max_hora_orders<0: max_hora_orders= 23
+
+# %% Df Order Previsão
+
+
+query_order_previsao  = "select \
+DATE_FORMAT(ord.order_datetime,'%Y-%m-%d %H:00:00') as DateHour,\
+Date(ord.order_datetime) as Data,\
+HOUR(ord.order_datetime) as Hora,\
+CONVERT(ord.id, char) as order_id,\
+CONVERT(ord_ite.id, char) as order_item_id,\
+ord.customer_id, \
+ord.region_id, \
+CASE WHEN cli.region_id in (1,7,19,27,28,29,30,31,36,37)  THEN 'RJC' \
+WHEN cli.region_id in (22,24,25) THEN 'RJI' \
+WHEN cli.region_id in (16,26,49,50,51,52,53) THEN 'BAC' ELSE ord.region_id END as 'Região',\
+ord_ite.store_id, \
+CONVERT(ord_ite.product_id, CHAR) as ean,\
+CONVERT(ord_ite.unit_product_id, CHAR) as unit_ean,\
+prod.description as Produto,\
+ord_ite.category as Categoria,\
+ord_ite.is_multi_package,\
+ord_ite.product_package_qtd,\
+ord_ite.price_managers,\
+Convert(ord_ite.offer_id, char) as offer_id,  \
+case when ord_ite.original_price > ord_ite.unit_price then 1 else 0 end as flag_desconto,\
+case when  ord_ite.is_multi_package = 0 then  COALESCE(ord_ite.original_price,ord_ite.unit_price) else COALESCE(ord_ite.original_price,ord_ite.unit_price)/ ord_ite.product_package_qtd  end as Original_Price, \
+case when  ord_ite.is_multi_package = 0 then  ord_ite.unit_price else  ord_ite.unit_price/ ord_ite.product_package_qtd  end as Price, \
+case when ord_ite.original_price > ord_ite.unit_price then \
+case when  ord_ite.is_multi_package = 0 then  COALESCE(ord_ite.original_price,ord_ite.unit_price) else COALESCE(ord_ite.original_price,ord_ite.unit_price)/ ord_ite.product_package_qtd end - \
+case when  ord_ite.is_multi_package = 0 then  ord_ite.unit_price else  ord_ite.unit_price/ ord_ite.product_package_qtd  end \
+else 0 end as desconto_unitario, \
+case when ord_ite.original_price > ord_ite.unit_price then \
+(case when  ord_ite.is_multi_package = 0 then  COALESCE(ord_ite.original_price,ord_ite.unit_price) else COALESCE(ord_ite.original_price,ord_ite.unit_price)/ ord_ite.product_package_qtd end - \
+case when  ord_ite.is_multi_package = 0 then  ord_ite.unit_price else  ord_ite.unit_price/ ord_ite.product_package_qtd  end ) * \
+case \
+when ord_ite.product_package_qtd  is null then ord_ite.quantity \
+when ord_ite.product_package_qtd  <=0 then ord_ite.quantity \
+else ord_ite.product_package_qtd  * ord_ite.quantity end \
+else 0 end as desconto_total, \
+case \
+when ord_ite.product_package_qtd  is null then ord_ite.quantity \
+when ord_ite.product_package_qtd  <=0 then ord_ite.quantity \
+else ord_ite.product_package_qtd  * ord_ite.quantity end as Quantity, \
+ord_ite.quantity *  \
+(CASE WHEN prod.gross_weight_in_gram IS NOT NULL THEN prod.gross_weight_in_gram  WHEN prod.net_volume_in_liters IS NOT NULL AND cat.gross_weight_per_content_volume_liter IS NOT NULL THEN COALESCE(prod.number_of_items, 1) * prod.net_volume_in_liters * cat.gross_weight_per_content_volume_liter  \
+WHEN prod.net_weight_in_gram IS NOT NULL AND cat.gross_weight_per_net_weight_gram IS NOT NULL THEN COALESCE(prod.number_of_items, 1) * prod.net_weight_in_gram * cat.gross_weight_per_net_weight_gram  \
+WHEN cat.category_id IS NOT NULL THEN COALESCE(cat.average_without_outliers, cat.average) * COALESCE(prod.number_of_items, 1)  \
+WHEN prod.net_weight_in_gram IS NOT NULL THEN prod.net_weight_in_gram  \
+WHEN prod.net_volume_in_liters IS NOT NULL THEN prod.net_volume_in_liters * 1000 ELSE 1000  END) / 1000.0 as 'Peso',\
+ord_ite.total_price  as 'Gmv' \
+from  clubbi_backend.order ord \
+left join clubbi_backend.order_item ord_ite on ord_ite.order_id = ord.id and (ord_ite.is_cancelled = 0 or ord_ite.is_cancelled IS NULL) \
+left join clubbi.product prod ON ord_ite.product_id = prod.ean \
+left join  clubbi.merchants  cli on cli.client_site_code = ord.customer_id \
+left join clubbi.category_volume cat ON prod.category_id = cat.category_id  \
+where    \
+1 = 1 \
+and DATE(ord.order_datetime) <  CURDATE()  \
+;"\
+
+
+query_produtos_previsao  = "select convert(prod.ean,char) as ean ,prod.description,prod.category_id, prod.unit_ean, prod.only_sell_package, cat.category as Categoria, cat.section  from clubbi.product prod left join clubbi.category cat on cat.id = prod.category_id ;"
+ 
+ 
+@st.cache_resource( ttl = 45000) 
+def load_produtos_previsao():
+    mydb = load_my_sql()  
+    query_produtos  = pd.read_sql(query_produtos_previsao,mydb)  
+    query_produtos['ean'] = query_produtos['ean'].astype(np.int64).astype(str)
+    return query_produtos
+
+df_produtos_previsao = load_produtos_previsao()
+ 
+df_produtos_previsao = df_produtos_previsao.rename(columns={'ean':'unit_ean_prod','description':'Unit_Description'})[['unit_ean_prod','Unit_Description','Categoria']]  
+
+@st.cache_resource( ttl = 45000) 
+def load_orders_previsao():
+    mydb = load_my_sql() 
+    query_orders = pd.read_sql(query_order_previsao,mydb)  
+        
+    df_produtos = load_produtos_previsao()
+ 
+    df_inicial = query_orders.copy()
+
+    df_inicial['Quantity'] = df_inicial['Quantity'].replace(np.nan,0)
+    df_inicial = df_inicial[df_inicial['Quantity'] > 0]
+
+    df_inicial['ean'] = df_inicial['ean'].astype(np.int64).astype(str) 
+    df_inicial['unit_ean'] = df_inicial['unit_ean'].astype(np.int64).astype(str) 
+    
+
+    df_inicial = df_inicial.drop(columns = ['Categoria'])
+
+    
+    df_produtos = df_produtos.copy()
+    df_produtos['ean'] = df_produtos['ean'].astype(np.int64).astype(str)
+    df_produtos = df_produtos.rename(columns={'ean':'unit_ean_prod','description':'Unit_Description'})[['unit_ean_prod','Unit_Description','Categoria']]  
+
+
+
+    df_inicial = df_inicial.merge(df_produtos  ,how ='left', left_on='unit_ean', right_on='unit_ean_prod', suffixes=(False, False))
+    df_inicial['Categoria'] =   np.where((df_inicial['Categoria'] == 'Óleos, Azeites e Vinagres') ,  'Óleos, Azeites E Vinagres'  , df_inicial['Categoria'] )
+    df_inicial['price_managers'] = df_inicial['price_managers'].replace(np.nan, 0 )
+    df_inicial['offer_id'] = df_inicial['offer_id'].replace(np.nan, 0 ).astype(np.int64).astype(float)
+ 
+    return df_inicial
+
+df_order_previsao = load_orders_previsao()
  
